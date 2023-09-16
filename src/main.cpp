b414bb7fb11fc5ff8a0da37eca57237f0316def1
Sérgio Marques <dusk_marques@hotmail.com>
2023-09-16 20:39:38 +0100

    Redid organization of the project to a more standard and easily understandable format.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-#include "GeneralEnums.h"
+#include "general/data/GeneralEnums.h"
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
+
 	std::cout << "RaceSim Engineer online." << std::endl;
+	std::cout << "Enum test: " << Packets::PacketType::MotionPacket << std::endl;
 	return 0;
+
 }

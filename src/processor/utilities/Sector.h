fbc84040b08ab85d8989baa53487341d0c29d697
Sérgio Marques <dusk_marques@hotmail.com>
2025-09-17 08:24:25 +0100

    Sector utils should now be fully implemented and working

--- src/processor/utilities/Sector.h
+++ src/processor/utilities/Sector.h
@@ -16,11 +16,19 @@ namespace Processor {
             // Retrieves data about a sector of the track in which this position of a lap is located in
             static bool validate(const Lap::Internal::Sector& sector);
 
+            // Retrieves data about the sector of the track immediately preceding the one in which this position of a lap is located in
+            static Lap::Internal::Sector& getPreviousSectorByDistance(std::map<uint8_t, Lap::Internal::Sector>& sectors,
+                const uint32_t distance, bool& usePreviousLap);
+
             // Retrieves data about a sector of the track in which this position of a lap is located in
             static Lap::Internal::Sector& getSectorByDistance(std::map<uint8_t, Lap::Internal::Sector>& sectors, const uint32_t distance);
 
             // Retrieves data about a sector of the track based on its ID
             static Lap::Internal::Sector& getSectorById(std::map<uint8_t, Lap::Internal::Sector>& sectors, const uint8_t id);
+
+            // Retrieves data about the minisector of the track immediately preceding in which this position of a lap is located in
+            static Lap::Internal::Sector& getPreviousMiniSectorByDistance(std::map<uint8_t, Lap::Internal::Sector>& sectors,
+                const uint32_t distance, bool& usePreviousLap);
 
             // Retrieves data about a minisector of the track in which this position of a lap is located in
             static Lap::Internal::Sector& getMiniSectorByDistance(std::map<uint8_t, Lap::Internal::Sector>& sectors, const uint32_t distance);
